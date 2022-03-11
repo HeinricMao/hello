533b8cdd838f67d5b3b542ac91e8bf21fef62d5f
HeinricMao <101338462+HeinricMao@users.noreply.github.com>
2022-03-11 15:58:11 +0800

    pull text

--- src/Hello.java
+++ src/Hello.java
@@ -9,5 +9,6 @@ public class Hello {
         System.out.println("hot-fix commit2");
         System.out.println("push test");
         System.out.println("push test2");
+        System.out.println("pull test");
     }
 }
