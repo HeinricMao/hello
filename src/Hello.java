5ba2296ffc5ab2b1102ca3543dd660ff19945502
gcb <2387993505@qq.com>
2022-03-11 11:44:46 +0800

    master commit2

--- src/Hello.java
+++ src/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("git java2");
         System.out.println("git java3");
         System.out.println("git java4");
+        System.out.println("master commit2");
     }
 }
