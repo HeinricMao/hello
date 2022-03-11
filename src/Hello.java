d5e0284bc7248db644bbff016b9b31e5c368bd6b
gcb <2387993505@qq.com>
2022-03-11 11:14:24 +0800

    second commit

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello idea");
         System.out.println("git java");
+        System.out.println("git java2");
     }
 }
